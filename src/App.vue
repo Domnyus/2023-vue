369904beabdd46cc95d1cb80d712ef83bd1f33e3
Domnyus <domnyus@gmail.com>
2023-10-12 01:01:03 -0300

    2023-10-11/12

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, reactive, computed, onMounted, watch } from 'vue'
+import childComp from "./components/childComp.vue"
+
+const title = ref("Título")
+const subtitle = ref("nah")
+</script>
+
+<template>
+  <childComp>
+    b
+  </childComp>
+</template>
+
+<style></style>
---
+++ src/components/childComp.vue
@@ -0,0 +1,5 @@
+<template>
+    <slot>
+        Fall
+    </slot>
+</template>
